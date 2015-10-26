fee1d20f2ae12fc6cb3c81db76f5617ce89e540f
HL706 <michaelgormley48@gmail.com>
2015-10-26 20:25:50 +0000

    Created stub classes.
    Create stub classes for two prime number generation algorithms.

---
+++ PrimeNumberCodeTest/Utilities/IPrimeGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberCodeTest.Utilities
+{
+    interface IPrimeGenerator
+    {
+        List<uint> GeneratePrimes(int numberOfPrimesToGenerate);
+    }
+}
---
+++ PrimeNumberCodeTest/Utilities/SieveOfEratosthenes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberCodeTest.Utilities
+{
+    public class SieveOfEratosthenes : IPrimeGenerator
+    {
+        public SieveOfEratosthenes()
+        {
+
+        }
+
+        public List<uint> GeneratePrimes(int numberOfPrimesToGenerate)
+        {
+            return new List<uint>();
+        }
+    }
+}
